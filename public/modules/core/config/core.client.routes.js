e497dfa4a61332626d393ec6bd44b84e94807aaf
Jacob Jenkins <jacob@jacobejenkins.com>
2015-02-16 13:39:17 -0500

    Added some calendar code

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -8,6 +8,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
 		// Home state routing
 		$stateProvider.
+		state('calendar', {
+			url: '/calendar',
+			templateUrl: 'modules/core/views/calendar.client.view.html'
+		}).
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
