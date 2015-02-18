595f3b2a38fa28468dda6b54fb7dcc2ed90eb701
Jacob Jenkins <jacob@jacobejenkins.com>
2015-02-18 12:59:10 -0500

    Removed home dropdown

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,8 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+                'public/lib/bootstrap-social/bootstrap-social.css',
+                'public/lib/font-awesome/css/font-awesome.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
