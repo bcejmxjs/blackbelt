5043a62533ec6b23bcd7b1a50eb498f15d7f640b
Max Fresonke <max@fresonke.com>
2015-02-19 14:21:01 -0500

    Hours Open page is now fully dynamic.

--- public/modules/calendar/controllers/calendar.client.controller.js
+++ public/modules/calendar/controllers/calendar.client.controller.js
@@ -3,8 +3,75 @@
 angular.module('calendar').controller('CalendarController', ['$scope', 'Events',
     function($scope, Events) {
 		// Calendar controller logic
-		// ...
+
+		/* Common Variables */
         $scope.moment = moment;
+
+        /* Begin Open/Closed Code */
+
+        //TODO Gets Hours for Different days from DB.
+        //Define Open Days
+    	$scope.days = [
+        	{name:'Sunday', isOpen:false},
+        	{name:'Monday', isOpen:true, openHour:7, closeHour:18},
+        	{name:'Tuesday', isOpen:true, openHour:7, closeHour:18},
+        	{name:'Wednesday', isOpen:true, openHour:7, closeHour:18},
+        	{name:'Thursday', isOpen:true, openHour:7, closeHour:18},
+        	{name:'Friday', isOpen:true, openHour:7, closeHour:18},
+        	{name:'Saturday', isOpen:false}
+    	];
+
+    	//Takes in a day number and returns the correct style for the given day.
+    	$scope.getDayStyle = function( dayIndex )
+    	{
+    		if( moment().day() === dayIndex )
+    			return {'background':'whitesmoke'};
+    		else 
+    			return {};
+    	};
+
+    	//Determines currently open/closed status
+    	$scope.isCurrentlyOpen = function()
+    	{
+    		var day = $scope.days[moment().day()];
+    		if( day.isOpen && 
+    			moment().hour() >= day.openHour && 
+    			moment().hour() < day.closeHour 
+    			)
+    			return true;
+    		else 
+    			return false;
+    	};
+
+    	//Gets correct panel style to associate with being opened/closed.
+    	$scope.getOpenPanelClass = function()
+    	{
+    		if( $scope.isCurrentlyOpen() )
+    			return 'panel-success';
+    		else
+    			return 'panel-danger';
+
+    	};
+
+    	//Gets correct panel TEXT to associate with being opened/closed.
+    	$scope.getOpenPanelText = function()
+    	{
+    		if( $scope.isCurrentlyOpen() )
+    			return 'Currently Open';
+    		else
+    			return 'Currently Closed';
+    	};
+
+        //converts the hour from 24 hour time, to a human readable form.
+        $scope.hourFormat = function( hour )
+        {
+        	if( hour < 12 )
+        		return hour + ':00 AM';
+        	else
+        		return (hour - 12) + ':00 PM';
+        };        
+
+        /* Begin Events Code */
 
         $scope.list = function() {
             $scope.events = Events.query();
