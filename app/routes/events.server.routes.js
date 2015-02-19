f7547c31331235fcc74b08aaf4581be6af84b44e
Jacob Jenkins <jacob@jacobejenkins.com>
2015-02-18 22:19:24 -0500

    Renamed some files related to event backend logic for the sake of naming conventions.

--- app/routes/events.server.routes.js
+++ app/routes/events.server.routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var events = require('../../app/controllers/event.server.controller');
+var events = require('../../app/controllers/events.server.controller');
 
 module.exports = function(app) {
 	// Routing logic   
