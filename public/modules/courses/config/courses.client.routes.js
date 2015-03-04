eff39bfa0875a7a7ee832aee4744bbd7eb97a37c
Jacob Jenkins <jacob@jacobejenkins.com>
2015-03-03 20:59:08 -0500

    Added Videogular dependencies and a view and controller for lectures.

--- public/modules/courses/config/courses.client.routes.js
+++ public/modules/courses/config/courses.client.routes.js
@@ -5,6 +5,10 @@ angular.module('courses').config(['$stateProvider',
 	function($stateProvider) {
 		// Courses state routing
 		$stateProvider.
+		state('lecture', {
+			url: '/lecture',
+			templateUrl: 'modules/courses/views/lecture.client.view.html'
+		}).
 		state('course-list', {
 			url: '/courses',
 			templateUrl: 'modules/courses/views/course-list.client.view.html'
---
+++ public/modules/courses/controllers/lecture.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('courses').controller('LectureController', ['$scope',
+	function($scope) {
+		// Lecture controller logic
+		// ...
+	}
+]);
