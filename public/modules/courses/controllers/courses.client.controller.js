4a41240de29ea3e785d27975f3d537105f8b48a5
Xixi <flyskyxi@gmail.com>
2015-03-09 14:32:28 -0400

    Bootstrap UI edit courses
    need more editing

--- public/modules/courses/controllers/courses.client.controller.js
+++ public/modules/courses/controllers/courses.client.controller.js
@@ -1,151 +1,126 @@
 'use strict';
 
-//<<<<<<< Updated upstream
-angular.module('courses').controller('CoursesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Courses',
-    function($scope, $stateParams, $location, Authentication, Courses) {
-        $scope.authentication = Authentication;
+var courseApp = angular.module('courses');
 
-        // Create new Course
-        $scope.create = function() {
-            // Create new Course object
-            var course = new Courses({
-                name: this.name,
-                description: this.description,
-                price: this.price,
-                level: this.level,
-                instructor: this.instructor
-            });
+courseApp.controller('CoursesController', ['$scope', '$stateParams', 'Authentication', 'Courses', '$modal', '$log',
+    function($scope, $stateParams, Authentication, Courses, $modal, $log) {
+        
+        this.authentication = Authentication;
 
-            // Redirect after save
-            course.$save(function(response) {
-                $location.path('courses/' + response._id);
+         
+        this.courses = Courses.query();
 
-                // Clear form fields
-                $scope.name = '';
-                $scope.description = '';
-                $scope.price = '';
-                $scope.level = '';
-                $scope.instructor = '';
-            }, function(errorResponse) {
-                $scope.error = errorResponse.data.message;
-            });
-        };
+        // Open a modal window to update a single customer record
+        this.modalUpdate = function (size, selectedCourse) {
 
-        // Remove existing Course
-        $scope.remove = function(course) {
-            if (course) {
-                course.$remove();
-
-                for (var i in $scope.courses) {
-                    if ($scope.courses[i] === course) {
-                        $scope.courses.splice(i, 1);
+             var modalInstance = $modal.open({
+                 templateUrl: 'modules/courses/views/edit-course.client.view.html',
+                controller: ModalInstanceCtrl,
+                size: size,
+                resolve: {
+                    course: function () {
+                        return selectedCourse;
                     }
-                }
-            } else {
-                $scope.course.$remove(function() {
-                    $location.path('courses');
+                 }
+             });
+
+            modalInstance.result.then(function (selectedItem) {
+             $scope.selected = selectedItem;
+            }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
                 });
-            }
+         
         };
 
-        // Update existing Course
-        $scope.update = function() {
-            var course = $scope.course;
 
-            course.$update(function() {
-                $location.path('courses/' + course._id);
-            }, function(errorResponse) {
-                $scope.error = errorResponse.data.message;
-            });
-        };
+        var ModalInstanceCtrl = function ($scope, $modalInstance, course){
+             $scope.course = course;
+        }
 
-        // Find a list of Courses
-        $scope.find = function() {
-            $scope.courses = Courses.query();
-        };
 
-        // Find existing Course
-        $scope.findOne = function() {
-            $scope.course = Courses.get({
-                courseId: $stateParams.courseId
-            });
-        };
-    }
-// =======
-// // Courses controller
-//
-// var coursesApp = angular.module('courses');
-//
-//
-//
-//
-// coursesApp.controller('CoursesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Courses',
-// 	function($scope, $stateParams, $location, Authentication, Courses) {
-// 		$scope.authentication = Authentication;
-//
-// 		// Create new Course
-// 		$scope.create = function() {
-// 			// Create new Course object
-// 			var course = new Courses ({
-// 				name: this.name,
-// 				description: this.description,
-// 				price: this.price,
-// 				level: this.level
-// 			});
-//
-// 			// Redirect after save
-// 			course.$save(function(response) {
-// 				$location.path('courses/' + response._id);
-//
-// 				// Clear form fields
-// 				$scope.name = '';
-// 				$scope.description = '';
-// 				$scope.price = '';
-// 				$scope.level = '';
-// 			}, function(errorResponse) {
-// 				$scope.error = errorResponse.data.message;
-// 			});
-// 		};
-//
-// 		// Remove existing Course
-// 		$scope.remove = function(course) {
-// 			if ( course ) {
-// 				course.$remove();
-//
-// 				for (var i in $scope.courses) {
-// 					if ($scope.courses [i] === course) {
-// 						$scope.courses.splice(i, 1);
-// 					}
-// 				}
-// 			} else {
-// 				$scope.course.$remove(function() {
-// 					$location.path('courses');
-// 				});
-// 			}
-// 		};
-//
-// 		// Update existing Course
-// 		$scope.update = function() {
-// 			var course = $scope.course;
-//
-// 			course.$update(function() {
-// 				$location.path('courses/' + course._id);
-// 			}, function(errorResponse) {
-// 				$scope.error = errorResponse.data.message;
-// 			});
-// 		};
-//
-// 		// Find a list of Courses
-// 		$scope.find = function() {
-// 			$scope.courses = Courses.query();
-// 		};
-//
-// 		// Find existing Course
-// 		$scope.findOne = function() {
-// 			$scope.course = Courses.get({
-// 				courseId: $stateParams.courseId
-// 			});
-// 		};
-// 	}
-// >>>>>>> Stashed changes
-]);
+
+        
+   }    
+
+]);      
+
+courseApp.controller('CoursesCreateController', ['$scope', 'Courses',
+    function($scope, Courses) {
+
+   }
+]);  
+
+courseApp.controller('CoursesEditController', ['$scope', 'Courses',
+    function($scope, Courses) {
+
+   }
+]);    
+//         $scope.authentication = Authentication;
+
+//         // Create new Course
+//         $scope.create = function() {
+//             // Create new Course object
+//             var course = new Courses({
+//                 name: this.name,
+//                 description: this.description,
+//                 price: this.price,
+//                 level: this.level,
+//                 instructor: this.instructor
+//             });
+
+//             // Redirect after save
+//             course.$save(function(response) {
+//                 $location.path('courses/' + response._id);
+
+//                 // Clear form fields
+//                 $scope.name = '';
+//                 $scope.description = '';
+//                 $scope.price = '';
+//                 $scope.level = '';
+//                 $scope.instructor = '';
+//             }, function(errorResponse) {
+//                 $scope.error = errorResponse.data.message;
+//             });
+//         };
+
+//         // Remove existing Course
+//         $scope.remove = function(course) {
+//             if (course) {
+//                 course.$remove();
+
+//                 for (var i in $scope.courses) {
+//                     if ($scope.courses[i] === course) {
+//                         $scope.courses.splice(i, 1);
+//                     }
+//                 }
+//             } else {
+//                 $scope.course.$remove(function() {
+//                     $location.path('courses');
+//                 });
+//             }
+//         };
+
+//         // Update existing Course
+//         $scope.update = function() {
+//             var course = $scope.course;
+
+//             course.$update(function() {
+//                 $location.path('courses/' + course._id);
+//             }, function(errorResponse) {
+//                 $scope.error = errorResponse.data.message;
+//             });
+//         };
+
+//         // Find a list of Courses
+//         $scope.find = function() {
+//             $scope.courses = Courses.query();
+//         };
+
+//         // Find existing Course
+//         $scope.findOne = function() {
+//             $scope.course = Courses.get({
+//                 courseId: $stateParams.courseId
+//             });
+//         };
+//     }
+// ]);
