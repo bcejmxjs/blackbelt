708cf081e4afd7e2f35b5c2bc0014c78f9826109
Jacob Jenkins <jacob@jacobejenkins.com>
2015-03-22 23:35:17 -0400

    Added route for test lesson

--- public/modules/courses/config/courses.client.routes.js
+++ public/modules/courses/config/courses.client.routes.js
@@ -1,31 +1,35 @@
 'use strict';
 
 //Setting up route
-angular.module('courses').config(['$stateProvider','$sceDelegateProvider',
-	function($stateProvider, $sceDelegateProvider) {
-		// Courses state routing
-		$stateProvider.
-		state('listCourses', {
-			url: '/courses',
-			templateUrl: 'modules/courses/views/list-courses.client.view.html'
-		}).
-		state('createCourse', {
-			url: '/courses/create',
-			templateUrl: 'modules/courses/views/create-course.client.view.html'
-		}).
-		state('viewCourse', {
-			url: '/courses/:courseId',
-			templateUrl: 'modules/courses/views/view-course.client.view.html'
-		}).
-		state('editCourse', {
-			url: '/courses/:courseId/edit',
-			templateUrl: 'modules/courses/views/edit-course.client.view.html'
-		}).
-		state('RemoveCourse', {
-			url: '/courses/:courseId/remove',
-			templateUrl: 'modules/courses/views/remove-course.client.view.html'
-		});
+angular.module('courses').config(['$stateProvider', '$sceDelegateProvider',
+    function($stateProvider, $sceDelegateProvider) {
+        // Courses state routing
+        $stateProvider.
+        state('listCourses', {
+            url: '/courses',
+            templateUrl: 'modules/courses/views/list-courses.client.view.html'
+        }).
+        state('createCourse', {
+            url: '/courses/create',
+            templateUrl: 'modules/courses/views/create-course.client.view.html'
+        }).
+        state('viewCourse', {
+            url: '/courses/:courseId',
+            templateUrl: 'modules/courses/views/view-course.client.view.html'
+        }).
+        state('editCourse', {
+            url: '/courses/:courseId/edit',
+            templateUrl: 'modules/courses/views/edit-course.client.view.html'
+        }).
+        state('RemoveCourse', {
+            url: '/courses/:courseId/remove',
+            templateUrl: 'modules/courses/views/remove-course.client.view.html'
+        }).
+        state('Lesson', {
+            url: '/lesson',
+            templateUrl: 'modules/courses/views/lesson.client.view.html'
+        });
 
-		$sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$')]);
-	}
+        $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$')]);
+    }
 ]);
