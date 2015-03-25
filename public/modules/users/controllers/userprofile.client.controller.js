409d9cc2bcc1d4c2ad15087bec93bb8f5436bd9c
Jacob Jenkins <jacob@jacobejenkins.com>
2015-03-25 16:45:03 -0400

    Fixed an issue with the userprofile.

--- public/modules/users/controllers/userprofile.client.controller.js
+++ public/modules/users/controllers/userprofile.client.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('users').controller('UserprofileController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-	function($scope, $http, $location, Users, Authentication) {
-		$scope.user = Authentication.user;
+    function($scope, $http, $location, Users, Authentication) {
+        $scope.authentication = Authentication;
 
-		// If user is not signed in then redirect back home
-		if (!$scope.user) $location.path('/');
+        // If user is not signed in then redirect back home
+        if (!$scope.user) $location.path('/');
 
-	}
+    }
 ]);
