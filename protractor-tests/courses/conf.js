e2c70b43566a7d9d3a072dc94166061394222de2
corya14 <corya14@gmail.com>
2015-03-27 13:31:24 -0400

    Prep for courses testing
    The protractor:courses target is commented out in the grunt file for
    now.
    Also got rid of another browser.sleep

---
+++ protractor-tests/courses/conf.js
@@ -0,0 +1,32 @@
+//conf.js
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: [
+  	
+  ],
+  baseUrl: 'http://localhost:3000',
+
+  rootElement:'html',
+
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new
+    SpecReporter({displayFailuresSummary: false}));
+  },
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
