23e1c9f18ab669deba334c2da8c54b7a1dc6f903
Jacob Jenkins <jacob@jacobejenkins.com>
2015-03-27 19:09:33 -0400

    Added a sample lesson under Kenpo for testing things.

--- gruntfile.js
+++ gruntfile.js
@@ -1,179 +1,179 @@
 'use strict';
 
 module.exports = function(grunt) {
-	// Unified Watch Object
-	var watchFiles = {
-		serverViews: ['app/views/**/*.*'],
-		serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
-		clientViews: ['public/modules/**/views/**/*.html'],
-		clientJS: ['public/js/*.js', 'public/modules/**/*.js'],
-		clientCSS: ['public/modules/**/*.css'],
-		mochaTests: ['app/tests/**/*.js']
-	};
+    // Unified Watch Object
+    var watchFiles = {
+        serverViews: ['app/views/**/*.*'],
+        serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
+        clientViews: ['public/modules/**/views/**/*.html'],
+        clientJS: ['public/js/*.js', 'public/modules/**/*.js'],
+        clientCSS: ['public/modules/**/*.css'],
+        mochaTests: ['app/tests/**/*.js']
+    };
 
-	// Project Configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		watch: {
-			serverViews: {
-				files: watchFiles.serverViews,
-				options: {
-					livereload: true
-				}
-			},
-			serverJS: {
-				files: watchFiles.serverJS,
-				tasks: ['jshint'],
-				options: {
-					livereload: true
-				}
-			},
-			clientViews: {
-				files: watchFiles.clientViews,
-				options: {
-					livereload: true,
-				}
-			},
-			clientJS: {
-				files: watchFiles.clientJS,
-				tasks: ['jshint'],
-				options: {
-					livereload: true
-				}
-			},
-			clientCSS: {
-				files: watchFiles.clientCSS,
-				tasks: ['csslint'],
-				options: {
-					livereload: true
-				}
-			}
-		},
-		jshint: {
-			all: {
-				src: watchFiles.clientJS.concat(watchFiles.serverJS),
-				options: {
-					jshintrc: true
-				}
-			}
-		},
-		csslint: {
-			options: {
-				csslintrc: '.csslintrc',
-			},
-			all: {
-				src: watchFiles.clientCSS
-			}
-		},
-		uglify: {
-			production: {
-				options: {
-					mangle: false
-				},
-				files: {
-					'public/dist/application.min.js': 'public/dist/application.js'
-				}
-			}
-		},
-		cssmin: {
-			combine: {
-				files: {
-					'public/dist/application.min.css': '<%= applicationCSSFiles %>'
-				}
-			}
-		},
-		nodemon: {
-			dev: {
-				script: 'server.js',
-				options: {
-					nodeArgs: ['--debug'],
-					ext: 'js,html',
-					watch: watchFiles.serverViews.concat(watchFiles.serverJS)
-				}
-			}
-		},
-		'node-inspector': {
-			custom: {
-				options: {
-					'web-port': 1337,
-					'web-host': 'localhost',
-					'debug-port': 5858,
-					'save-live-edit': true,
-					'no-preload': true,
-					'stack-trace-limit': 50,
-					'hidden': []
-				}
-			}
-		},
-		ngAnnotate: {
-			production: {
-				files: {
-					'public/dist/application.js': '<%= applicationJavaScriptFiles %>'
-				}
-			}
-		},
-		concurrent: {
-			default: ['nodemon', 'watch'],
-			debug: ['nodemon', 'watch', 'node-inspector'],
-			options: {
-				logConcurrentOutput: true,
-				limit: 10
-			}
-		},
-		env: {
-			test: {
-				NODE_ENV: 'test'
-			},
-			secure: {
-				NODE_ENV: 'secure'
-			}
-		},
-		mochaTest: {
-			src: watchFiles.mochaTests,
-			options: {
-				reporter: 'spec',
-				require: 'server.js'
-			}
-		},
-		karma: {
-			unit: {
-				configFile: 'karma.conf.js'
-			}
-		},
-		// SAVE & LOAD MONGODB INSTANCES!
-		easy_mongo_fixture: {
-		  load: {
-		    options: {
-		      database: 'blackbelt-dev',
-		      dir: './mongo-fixtures',
-		      override: true
-		    },
-		    collections: ['users', 'events', 'courses'],
-		    action: 'load'
-		  },
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            serverViews: {
+                files: watchFiles.serverViews,
+                options: {
+                    livereload: true
+                }
+            },
+            serverJS: {
+                files: watchFiles.serverJS,
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            clientViews: {
+                files: watchFiles.clientViews,
+                options: {
+                    livereload: true,
+                }
+            },
+            clientJS: {
+                files: watchFiles.clientJS,
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            clientCSS: {
+                files: watchFiles.clientCSS,
+                tasks: ['csslint'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jshint: {
+            all: {
+                src: watchFiles.clientJS.concat(watchFiles.serverJS),
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc',
+            },
+            all: {
+                src: watchFiles.clientCSS
+            }
+        },
+        uglify: {
+            production: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    'public/dist/application.min.js': 'public/dist/application.js'
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'public/dist/application.min.css': '<%= applicationCSSFiles %>'
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    ext: 'js,html',
+                    watch: watchFiles.serverViews.concat(watchFiles.serverJS)
+                }
+            }
+        },
+        'node-inspector': {
+            custom: {
+                options: {
+                    'web-port': 1337,
+                    'web-host': 'localhost',
+                    'debug-port': 5858,
+                    'save-live-edit': true,
+                    'no-preload': true,
+                    'stack-trace-limit': 50,
+                    'hidden': []
+                }
+            }
+        },
+        ngAnnotate: {
+            production: {
+                files: {
+                    'public/dist/application.js': '<%= applicationJavaScriptFiles %>'
+                }
+            }
+        },
+        concurrent: {
+            default: ['nodemon', 'watch'],
+            debug: ['nodemon', 'watch', 'node-inspector'],
+            options: {
+                logConcurrentOutput: true,
+                limit: 10
+            }
+        },
+        env: {
+            test: {
+                NODE_ENV: 'test'
+            },
+            secure: {
+                NODE_ENV: 'secure'
+            }
+        },
+        mochaTest: {
+            src: watchFiles.mochaTests,
+            options: {
+                reporter: 'spec',
+                require: 'server.js'
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        // SAVE & LOAD MONGODB INSTANCES!
+        easy_mongo_fixture: {
+            load: {
+                options: {
+                    database: 'blackbelt-dev',
+                    dir: './mongo-fixtures',
+                    override: true
+                },
+                collections: ['users', 'events', 'courses', 'lessons'],
+                action: 'load'
+            },
 
-		  save: {
-		    options: {
-		      database: 'blackbelt-dev',
-		      dir: './mongo-fixtures',
-		      override: true
-		    },
-		    collections: ['users', 'events', 'courses'],
-		    action: 'save'
-		  }
-		},
-		protractor: {
-		      options: {
-		        configFile: 'node_modules/protractor/example/conf.js', // Default config file
-		        keepAlive: false, // If false, the grunt process stops when the test fails.
-		        noColor: false, // If true, protractor will not use colors in its output.
-		        verbose: true,
-		        args: {
-		          // Arguments passed to the command
-		          chromeDriver: 'node_modules/protractor/selenium/chromedriver'
-		        }
-		      },
-		      /*
+            save: {
+                options: {
+                    database: 'blackbelt-dev',
+                    dir: './mongo-fixtures',
+                    override: true
+                },
+                collections: ['users', 'events', 'courses', 'lessons'],
+                action: 'save'
+            }
+        },
+        protractor: {
+            options: {
+                configFile: 'node_modules/protractor/example/conf.js', // Default config file
+                keepAlive: false, // If false, the grunt process stops when the test fails.
+                noColor: false, // If true, protractor will not use colors in its output.
+                verbose: true,
+                args: {
+                    // Arguments passed to the command
+                    chromeDriver: 'node_modules/protractor/selenium/chromedriver'
+                }
+            },
+            /*
 		      example: {
 		        options: {
 		          verbose: true,
@@ -183,21 +183,21 @@ module.exports = function(grunt) {
 		        }
 		      },
 		      */
-		      login: {
-		      	verbose: true,
-		      	keepAlive: false,
-		      	configFile: 'protractor-tests/login/conf.js',
-		      	// An example arg would be specifying a test suite
-		      	// Unsure of arg syntax
-		      	args: {}
-		      },
-		      calendar: {
-		      	verbose: true,
-		      	keepAlive: false,
-		      	configFile: 'protractor-tests/calendar/conf.js',
-		      	args: {}
-		      }//,
-		      /*
+            login: {
+                verbose: true,
+                keepAlive: false,
+                configFile: 'protractor-tests/login/conf.js',
+                // An example arg would be specifying a test suite
+                // Unsure of arg syntax
+                args: {}
+            },
+            calendar: {
+                verbose: true,
+                keepAlive: false,
+                configFile: 'protractor-tests/calendar/conf.js',
+                args: {}
+            } //,
+            /*
 		      courses: {
 		      	verbose: true,
 		      	keepAlive: false,
@@ -205,45 +205,45 @@ module.exports = function(grunt) {
 		      	args: {}		      	
 		      }
 		      */
-		    }
-	});
+        }
+    });
 
-	// Load NPM tasks
-	require('load-grunt-tasks')(grunt);
+    // Load NPM tasks
+    require('load-grunt-tasks')(grunt);
 
-	// Making grunt default to force in order not to break the project.
-	grunt.option('force', true);
+    // Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
 
-	// A Task for loading the configuration object
-	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
-		var init = require('./config/init')();
-		var config = require('./config/config');
+    // A Task for loading the configuration object
+    grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
+        var init = require('./config/init')();
+        var config = require('./config/config');
 
-		grunt.config.set('applicationJavaScriptFiles', config.assets.js);
-		grunt.config.set('applicationCSSFiles', config.assets.css);
-	});
+        grunt.config.set('applicationJavaScriptFiles', config.assets.js);
+        grunt.config.set('applicationCSSFiles', config.assets.css);
+    });
 
-	// Default task(s).
-	grunt.registerTask('default', ['lint', 'concurrent:default']);
+    // Default task(s).
+    grunt.registerTask('default', ['lint', 'concurrent:default']);
 
-	// Debug task.
-	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
+    // Debug task.
+    grunt.registerTask('debug', ['lint', 'concurrent:debug']);
 
-	// Secure task(s).
-	grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
+    // Secure task(s).
+    grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
 
-	// Lint task(s).
-	grunt.registerTask('lint', ['jshint', 'csslint']);
+    // Lint task(s).
+    grunt.registerTask('lint', ['jshint', 'csslint']);
 
-	// Build task(s).
-	grunt.registerTask('build', ['lint', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
+    // Build task(s).
+    grunt.registerTask('build', ['lint', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
 
-	// Test task.
-	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+    // Test task.
+    grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 
-	//SAVE AND LOAD MONGODB INSTANCES TASK
-	grunt.registerTask('mongo:load', ['easy_mongo_fixture:load']);
-	grunt.registerTask('mongo:save', ['easy_mongo_fixture:save']);
+    //SAVE AND LOAD MONGODB INSTANCES TASK
+    grunt.registerTask('mongo:load', ['easy_mongo_fixture:load']);
+    grunt.registerTask('mongo:save', ['easy_mongo_fixture:save']);
 
-	grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-protractor-runner');
 };
