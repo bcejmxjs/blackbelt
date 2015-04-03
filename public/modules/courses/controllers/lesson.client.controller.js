3f3ead8064b62799e188c00cf48c70676be1a7d2
Jacob Jenkins <jacob@jacobejenkins.com>
2015-04-03 01:42:58 -0400

    Started working more on lesson page.

--- public/modules/courses/controllers/lesson.client.controller.js
+++ public/modules/courses/controllers/lesson.client.controller.js
@@ -1,15 +1,14 @@
 'use strict';
 
-angular.module('courses').controller('LectureController', ['$scope', '$sce', '$stateParams', 'Lessons',
-    function($scope, $sce, $stateParams, Lessons) {
+angular.module('courses').controller('LectureController', ['$scope', '$sce', '$stateParams', '$modal', '$state', 'Lessons',
+    function($scope, $sce, $stateParams, $modal, $state, Lessons) {
         window.MY_SCOPE = $scope;
 
         $scope.lesson = Lessons.get({
             lessonId: $stateParams.lessonId
         });
 
-        // Lecture controller logic
-        // ...
+        // Configuration for video container.
         $scope.config = {
             sources: [{
                 src: $sce.trustAsResourceUrl("videos/big_buck_bunny.mp4"),
@@ -35,6 +34,41 @@ angular.module('courses').controller('LectureController', ['$scope', '$sce', '$s
             responsive: true,
             height: 380,
             width: 740
+        };
+
+        // Open a modal window to Remove a single lesson record
+        $scope.modalUserUpload = function(size, selectedLesson) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modules/lessons/views/lesson-remove.client.view.html',
+                controller: ModalUserUploadController,
+                size: size,
+                resolve: {
+                    lesson: function() {
+                        return selectedLesson;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(selectedItem) {
+                $scope.selected = selectedItem;
+            }, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+
+        };
+
+        var ModalRemoveCtrl = function($scope, $modalInstance, lesson) {
+            $scope.lesson = lesson;
+
+            $scope.ok = function() {
+                $modalInstance.close($scope.lesson);
+                $state.reload();
+            };
+
+            $scope.cancel = function() {
+                $modalInstance.dismiss('cancel');
+            };
         };
     }
 ]);
